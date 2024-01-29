c42e5e8eb2b3a0a374551ff7e2b3db437d193dd1
MAIRA CAROLINA SARAIVA GONCALVES <142128191+mcsgon@users.noreply.github.com>
2024-01-29 16:37:44 -0300

    Delete Modulo18 directory

--- Modulo18/Solução1/Model/Services/TaxService.java
+++
@@ -1,5 +0,0 @@
-package Modulo18.Solução1.Model.Services;
-
-public interface TaxService {
-    double tax(double amount); //método que deve ser usado por todas as casses que implementam a interface
-}
