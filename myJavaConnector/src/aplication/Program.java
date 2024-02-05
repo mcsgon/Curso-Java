37dc393de49606c93fac445fef236f8819a8e238
MAIRA CAROLINA SARAIVA GONCALVES <142128191+mcsgon@users.noreply.github.com>
2024-02-05 15:33:24 -0300

    Add files via upload

---
+++ myJavaConnector/src/aplication/Program.java
@@ -0,0 +1,11 @@
+package aplication;
+
+import java.sql.Connection;
+import db.DB;
+public class Program {
+    public static void main(String[] args) {
+        Connection conn=DB.getConnection();
+        DB.closeConnection();
+
+    }
+}
